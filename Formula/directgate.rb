beb71377352ad8792bfc70099459233adce8d734
Sandro Kalatozishvili <s.kalatoz@gmail.com>
2026-06-13 04:29:04 +0400

    Release directgate 1.0.19-5

--- Formula/directgate.rb
+++ Formula/directgate.rb
@@ -1,11 +1,11 @@
 class Directgate < Formula
   desc "DirectGate secure remote agent"
-  homepage "https://github.com/directgate/directgate-agent"
+  homepage "https://github.com/directgate/directgate"
   url "https://pkg.directgate.io/brew/directgate-1.0.19.tar.gz"
   version "1.0.19"
-  sha256 "d99cffbf59bd19def2b0af2a965767258d82ad4bdeda365a5b5d7d0f8f50fb2c"
+  sha256 "c71a294c4b8e717fcd20b64cc9a21aec8b8fee68bd07b491c5c1d76d141e32aa"
   license "GPL-3.0-or-later"
-  revision 3
+  revision 4
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
@@ -13,7 +13,7 @@ class Directgate < Formula
 
   resource "libdatachannel" do
     url "https://pkg.directgate.io/brew/libdatachannel-28b2e730f4c7.tar.gz"
-    sha256 "697e119b049bd5ed8dded30c01e13bef7f77eebd2c0c359dcd663296a4075e9d"
+    sha256 "df5f919014ea26270d003f1850e4ee39c80f8322fb61f9a2d5af2e8abe568a89"
   end
 
   def install
@@ -23,8 +23,13 @@ class Directgate < Formula
               '#define DIRECTGATE_LOG_PATH_DEFAULT "/var/log/directgate"',
               "#define DIRECTGATE_LOG_PATH_DEFAULT \"#{var}/log/directgate\""
 
+    # Build channel tag appended to the agent version (1.0.19-5-brew_silicon).
+    # Decided here because brew compiles on the user's machine, not in CI.
+    build_tag = Hardware::CPU.arm? ? "brew_silicon" : "brew_intel"
+
     system "cmake", "-S", ".", "-B", "build",
            "-DCMAKE_BUILD_TYPE=Release",
+           "-DDIRECTGATE_BUILD_TAG=#{build_tag}",
            "-DOPENSSL_ROOT_DIR=#{Formula["openssl@3"].opt_prefix}"
     system "cmake", "--build", "build"
 
